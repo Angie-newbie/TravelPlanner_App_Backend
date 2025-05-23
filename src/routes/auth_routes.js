22f2e0b06f27c3148da068033a61979bcb97d482
Jessica Amy (jesso) <jessicaannetteamy@gmail.com>
2025-05-23 14:15:38 +0930

    Dry error handling

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -12,10 +12,12 @@ import bcrypt from 'bcrypt'
 
 const router = Router()
 
+// re-wrote parts of this code to work with dry concepts with functions instead of repeating code.
 
 
 
 import User from '../models/user.js'
+import { badRequest, goodRequest, notFound} from '../utils/responses.js'
 
 // Login
 router.post('/login', async (req, res) => {
@@ -33,14 +35,14 @@ router.post('/login', async (req, res) => {
                 }, secret)
                 res.send({ token, email: user.email, accountType: user.accountType})
             } else {
-                res.status(404).send({ error: 'Email or password incorrect' })
+                notFound({ message: 'Email or password incorrect' })
             }
         } else {
-            res.status(404).send({ error: 'Email or password incorrect' })
+            notFound({ message: 'Email or password incorrect' })
         }
     }
     catch (err) {
-        res.status(400).send({ error: err.message })
+        badRequest({ message: err.message })
     }
 })
 
@@ -51,7 +53,8 @@ router.post('/register', auth, verifyToken, async (req, res) => {
 
         // we check if the values are entered
         if (!req.body.email || !req.body.password) {
-            return res.status(400).send({ error: 'Email and password are required' })
+            badRequest(res, message = 'Email and password are required')
+            //return res.status(400).send({ error: 'Email and password are required' })
         }
 
         // now we check user type then create the user
@@ -65,10 +68,19 @@ router.post('/register', auth, verifyToken, async (req, res) => {
     
         // Send user to the client with 201 status
         // TODO: Create a JWT so the user is automatically logged in
-        res.status(201).send({ email: user.email, accountType: user.accountType })
+        
+
+
+        // res.status(201).send({ email: user.email, accountType: user.accountType })
     
+        // lets create a 201 status response
+        //goodRequest(email: user.email, accountType: user.accountType, message = 'User created successfully')
+        // lets come back to this one
+        goodRequest(res, email = user.email, accountType = user.accountTypem, message = 'User created successfully')
+
+
     }catch (err) {
-        res.status(400).send({ error: err.message })
+        badRequest({ message: err.message })
     }
 
 })
--- src/utils/responses.js
+++ src/utils/responses.js
@@ -7,9 +7,19 @@ export function badRequest(res, message = 'Bad request') {
   return res.status(400).send({ error: message });
 };
 
+export function notFound(res, message = '404 Not Found') {
+  return res.status(404).send({ error: message });
+};
+
 export function serverError(res, message = 'Something went wrong on the server') {
   return res.status(500).send({ error: message });
 };
+
+// 201 success response
+
+export function goodRequest(res, email, userAccount, message = 'Good request') {
+  return res.status(201).send({email, userAccount, message: message });
+}
 
 // format Mongoose validation errors
 // the original error was too long
