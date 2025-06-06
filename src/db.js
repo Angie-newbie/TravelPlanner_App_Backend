e2a0e711f2e93cad90191201bf9dd0a3c5873819
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-06-07 00:49:13 +1000

    remove parsers from db.js

--- src/db.js
+++ src/db.js
@@ -23,10 +23,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB Atlas');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
