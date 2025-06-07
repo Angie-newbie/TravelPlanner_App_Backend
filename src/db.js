d90d785ddc68643b80f8a36212c611d6658f216b
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-06-07 10:50:20 +1000

    chore: repo reset to 'localhost' connection

--- src/db.js
+++ src/db.js
@@ -1,37 +1,19 @@
 // import mongoose from 'mongoose'
 const mongoose = require('mongoose');
 
-// Section to connect to the database locally
 
-// // Connect to MongoDB
-// async function connect() {
-// await mongoose.connect('mongodb://127.0.0.1:27017/travelp') // CHOOSE DB NAME
-// console.log(mongoose.connection.readyState == 1 ? 'Mongoose connected' : 'Mongoose failed to connect!')
-// }
+// Connect to MongoDB
+async function connect() {
+await mongoose.connect('mongodb://127.0.0.1:27017/travelp') // CHOOSE DB NAME
+console.log(mongoose.connection.readyState == 1 ? 'Mongoose connected' : 'Mongoose failed to connect!')
+}
 
-// // Disconnect from MongoDB
+// Disconnect from MongoDB
 
-// async function close() {
-//   await mongoose.disconnect()
-//   console.log(mongoose.connection.readyState == 0 ? 'Mongoose disconnected!' : 'Mongoose failed to disconnect!')
-// }
+async function close() {
+  await mongoose.disconnect()
+  console.log(mongoose.connection.readyState == 0 ? 'Mongoose disconnected!' : 'Mongoose failed to disconnect!')
+}
 
-// // Best practice export
-// module.exports = { connect, close }
-
-// Section to connect to the MongoDB Atlas
-
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to MongoDB Atlas');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    process.exit(1);
-  }
-};
-
-module.exports = { connect };
-
-
-
+// Best practice export
+module.exports = { connect, close }
